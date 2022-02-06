38abb2e4f49740dfe70db021148c777200f779a9
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-06 11:12:39 +0800

    added task fetching to task component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,27 +1,11 @@
 import { Container } from "@mui/material";
-import { useEffect } from "react";
-import { getTasks } from "../redux/features/task/taskSlice";
-import { useAppDispatch } from "../redux/store";
 import AddTask from "./AddTask";
 import Tasks from "./Tasks";
 import Timer from "./Timer";
 
 const Main: React.FC = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        await dispatch(getTasks());
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchTasks();
-  }, [dispatch]);
   return (
-    <Container maxWidth="xs" sx={{ background: "lightgrey", py: 10 }}>
+    <Container maxWidth="xs" sx={{ background: "lightgrey", minHeight: "94.5vh", pt: 10 }}>
       <Timer />
       <Tasks />
       <AddTask />
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
+import { getTasks } from "../redux/features/task/taskSlice";
+import { RootState, useAppDispatch } from "../redux/store";
 import TaskCard from "./TaskCard";
 
 const Tasks: React.FC = () => {
   const { data } = useSelector((state: RootState) => state.tasks);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getTasks());
+  }, [dispatch]);
 
   return <>{data && data.map((task) => <TaskCard task={task} key={task._id} />)}</>;
 };
