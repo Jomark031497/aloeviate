e0afe912d8bef114d60cf9cd38fd450c61ad69cc
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-07 06:16:12 +0800

    changed redirect useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Main from "./components/Main";
 import Register from "./components/Register";
+import { setCurrentUser } from "./redux/features/auth/authSlice";
+import { useAppDispatch } from "./redux/store";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const { data } = await axios.get("/api/auth/me");
+        dispatch(setCurrentUser(data));
+      } catch (error) {
+        console.log("may error");
+        navigate("/login");
+      }
+    };
+    checkAuth();
+  }, [dispatch, navigate]);
+
   return (
     <>
       <Header />
