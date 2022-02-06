cb3a9059028e06f61f16ffec64c1659c0e864076
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-06 18:26:28 +0800

    themed main container

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -28,7 +28,16 @@ const Main: React.FC = () => {
   }, [dispatch, navigate]);
 
   return (
-    <Container maxWidth="xs" sx={{ background: "lightgrey", minHeight: "94.5vh", pt: 10 }}>
+    <Container
+      maxWidth="xs"
+      sx={{
+        minHeight: "85vh",
+        background: "linear-gradient(40deg, rgba(96,55,85,1) 0%, rgba(75,73,122,1) 59%, rgba(14,80,144,1) 100%)",
+        borderRadius: "1rem",
+        width: { xs: "90vw" },
+        pt: 10,
+      }}
+    >
       {data && (
         <>
           <Timer />
