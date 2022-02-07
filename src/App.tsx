eb9a1154bbc54cf7ab1e49f187fce8964ae27441
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-07 09:53:46 +0800

    conditional rerouting for main and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Main from "./components/Main";
 import Register from "./components/Register";
+import { setCurrentUser } from "./redux/features/auth/authSlice";
+import { RootState, useAppDispatch } from "./redux/store";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { data: user } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const { data } = await axios.get("/api/auth/me");
+        dispatch(setCurrentUser(data));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    checkAuth();
+  }, [dispatch]);
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={user ? <Main /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,30 +1,9 @@
 import { Container } from "@mui/material";
-import axios from "axios";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { setCurrentUser } from "../redux/features/auth/authSlice";
-import { useAppDispatch } from "../redux/store";
 import AddTask from "./AddTask";
 import TasksContainer from "./TasksContainer";
 import Timer from "./Timer";
 
 const Main: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const { data } = await axios.get("/api/auth/me");
-        dispatch(setCurrentUser(data));
-      } catch (error) {
-        navigate("/login");
-      }
-    };
-    checkAuth();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <Container
       maxWidth="xs"
