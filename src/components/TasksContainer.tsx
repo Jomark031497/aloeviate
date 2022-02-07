c61a69b61f0e4c27dfb13f6aa6974d11f9042f6e
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-07 10:20:44 +0800

    removed task skeleton

--- src/components/TasksContainer.tsx
+++ src/components/TasksContainer.tsx
@@ -3,12 +3,10 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getTasks } from "../redux/features/task/taskSlice";
 import { RootState, useAppDispatch } from "../redux/store";
-import LoadingSkeleton from "./custom/LoadingSkeleton";
 import TaskCard from "./TaskCard";
 
 const Tasks: React.FC = () => {
   const { data: tasks } = useSelector((state: RootState) => state.tasks);
-  const { isLoading } = useSelector((state: RootState) => state.tasks);
 
   const dispatch = useAppDispatch();
 
@@ -18,34 +16,30 @@ const Tasks: React.FC = () => {
 
   return (
     <>
-      {isLoading ? (
-        <LoadingSkeleton />
-      ) : (
-        <Box id="tasks-container" sx={{ mx: "auto" }}>
-          {tasks.length ? (
-            <>
-              <Box>
-                {tasks &&
-                  tasks.map((task) => {
-                    return task.isCompleted ? null : <TaskCard task={task} key={task._id} />;
-                  })}
-              </Box>
-              <Divider sx={{ width: 260, m: "1rem auto" }} />
-              <Typography align="center" color="textSecondary">
-                Completed tasks
-              </Typography>
-              <Box>
-                {tasks &&
-                  tasks.map((task) => {
-                    return task.isCompleted ? <TaskCard task={task} key={task._id} /> : null;
-                  })}
-              </Box>
-            </>
-          ) : (
-            <Typography align="center">No tasks available</Typography>
-          )}
-        </Box>
-      )}
+      <Box id="tasks-container" sx={{ mx: "auto" }}>
+        {tasks.length ? (
+          <>
+            <Box>
+              {tasks &&
+                tasks.map((task) => {
+                  return task.isCompleted ? null : <TaskCard task={task} key={task._id} />;
+                })}
+            </Box>
+            <Divider sx={{ width: 260, m: "1rem auto" }} />
+            <Typography align="center" color="textSecondary">
+              Completed tasks
+            </Typography>
+            <Box>
+              {tasks &&
+                tasks.map((task) => {
+                  return task.isCompleted ? <TaskCard task={task} key={task._id} /> : null;
+                })}
+            </Box>
+          </>
+        ) : (
+          <Typography align="center">No tasks available</Typography>
+        )}
+      </Box>
     </>
   );
 };
