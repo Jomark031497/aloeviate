724ca6e0903a981187c2faab959cf9bf17a366a3
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-11 09:08:19 +0800

    fixed height of the main container

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
 import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
 import AddTask from "./AddTask";
 import TasksContainer from "./TasksContainer";
 import Timer from "./Timer";
@@ -9,22 +8,15 @@ const Main: React.FC = () => {
     <Container
       maxWidth="xs"
       sx={{
-        minHeight: "90vh",
+        minHeight: "85vh",
+        background: "linear-gradient(40deg, rgba(96,55,85,1) 0%, rgba(75,73,122,1) 59%, rgba(14,80,144,1) 100%)",
+        borderRadius: "1rem",
+        py: "3rem",
       }}
     >
-      <Box
-        sx={{
-          background: "linear-gradient(40deg, rgba(96,55,85,1) 0%, rgba(75,73,122,1) 59%, rgba(14,80,144,1) 100%)",
-          minHeight: "85vh",
-          borderRadius: "1rem",
-          py: "2rem",
-          my: "1rem",
-        }}
-      >
-        <Timer />
-        <TasksContainer />
-        <AddTask />
-      </Box>
+      <Timer />
+      <TasksContainer />
+      <AddTask />
     </Container>
   );
 };
