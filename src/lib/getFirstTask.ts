7dbcda7ba9959399589735e75f0af9e7976fc2d0
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-02-11 09:23:47 +0800

    removed colWrapper component; removed whitespaces

--- src/lib/getFirstTask.ts
+++ src/lib/getFirstTask.ts
@@ -1,16 +1,8 @@
 import { ITask } from "./types";
 
-export const getFirstTask = (tasks: ITask[]) => {
-  // check if there are tasks
+export default function getFirstTask(tasks: ITask[]) {
   if (!tasks.length) return;
-
-  // get all the incomplete tasks
   const filterIncompleteTasks = tasks.filter((task) => !task.isCompleted);
-
-  // check if there are filtered tasks
   if (!filterIncompleteTasks.length) return;
-
   return filterIncompleteTasks[0];
-};
-
-export default getFirstTask;
+}
